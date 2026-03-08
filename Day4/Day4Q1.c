14e4135d03da3db5778ee8702272363653cf3523
Radhikaa2410 <radhyashmann.0607@gmail.com>
2026-03-08 21:59:44 +0530

    100Days

--- Day4/Day4Q1.c
+++ Day4/Day4Q1.c
@@ -5,7 +5,7 @@ int main()
     printf("Enter n: ");
     scanf("%d", &n);
     printf("Enter elements:\n");
-    for (i = 0; i < n; i++) 
+    for (i = 0; i<n; i++) 
     {
         scanf("%d", &a[i]);
     }
