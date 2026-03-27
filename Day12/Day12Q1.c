2c2b13f55c3c1c18d824eee3fc632833a5d0af23
Radhikaa2410 <radhyashmann.0607@gmail.com>
2026-03-27 15:29:19 +0530

    100Days

--- Day12/Day12Q1.c
+++ Day12/Day12Q1.c
@@ -32,7 +32,8 @@ int main (){
 
     printf("Enter elements of the matrix (%d x %d):\n" , n ,n);
     for(int i = 0;i<n;i++){
-        for(int j=0;j<n;j++){
+        for(int j=0;j<n;j++)
+        {
             scanf("%d",&matrix[i][j]);
     
         }
